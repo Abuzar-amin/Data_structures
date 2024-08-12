14352b6e4e03c4f3fdc7ba59f66fdc030e6e8b9f
Abuzar-amin <133777283+Abuzar-amin@users.noreply.github.com>
2024-08-12 19:19:33 +0530

    Create leucas.c

---
+++ leucas.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int leucas(int n)
+{
+    if(n==1 || n==2 || n==3)
+    printf("0");
+    else
+    printf("%d",leucas(n-1)+leucas(n-2)+leucas(n-3));
+}
+int main()
+{
+   int n;
+   printf("Enter number of terms ");
+   scanf("%d",&n);
+   leucas(n);
+}
